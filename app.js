e38788f542b49414f2f2752609d1bcf6653dacd9
Vardaan <vardaan.agarwal.98@gmail.com>
2021-08-09 13:09:22 +0530

    add img

--- app.js
+++ app.js
@@ -2,13 +2,34 @@ const dobInput = document.querySelector("#dob");
 const luckyInput = document.querySelector("#lucky");
 const form = document.querySelector("#form");
 const btn = document.querySelector(".btn");
+const agreeDiv = document.querySelector(".agree-div");
+const disagreeDiv = document.querySelector(".disagree-div");
+
+// added an event listener that will play the onSubmit function when ubmit event is generated.
 
 form.addEventListener("submit", function onSubmit(e) {
   e.preventDefault();
 
-  const vanillaDOB = dobInput.value.replaceAll("-", "");
+  // the date is recieved in string format  with hyphens so in order to perform mathematical operations on the data we need to format it in number type and remove hyphens
+
+  let vanillaDOB = dobInput.value.replaceAll("-", "");
 
   console.log(vanillaDOB);
 
-  vanillaDOB = Number(vanillaDOB);
+  //to add elements inside array we initialise a counter
+
+  // then add this counter to the array index such that the number being added gets fed in the counter and the previous value is fed into counter statement to be added again as the loop progresses
+
+  let counter = 0;
+  for (value of vanillaDOB) {
+    counter = counter + Number(value);
+    console.log(counter);
+  }
+  if (counter % Number(luckyInput.value)) {
+    agreeDiv.style.display = "block";
+    disagreeDiv.style.display = "none";
+  } else {
+    disagreeDiv.style.display = "block";
+    agreeDiv.style.display = "none";
+  }
 });
